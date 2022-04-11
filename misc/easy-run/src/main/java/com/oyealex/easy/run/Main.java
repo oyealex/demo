9e947ec753bd474eefa132ea70b25792e462ae5e
oyealex <oyealex@outlook.com>
2022-04-11 15:18:06 +0100

    调整项目结构

---
+++ misc/easy-run/src/main/java/com/oyealex/easy/run/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2016 - 2022. oyealex. All rights reserved.
+ */
+
+package com.oyealex.easy.run;
+
+import java.nio.charset.Charset;
+
+/**
+ * Main
+ *
+ * @author oyealex
+ * @version 1.0
+ * @since 2022-03-28
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Charset.defaultCharset());
+    }
+}
