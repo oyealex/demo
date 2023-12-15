51efd8df41848084bd7d4a7faaca7c9b73644647
oyealex <oyealex@outlook.com>
2023-12-15 16:05:11 +0000

    add comprehensive rust code

---
+++ rust_demo/practice/comprehensive_rust/src/c2_control_flow.rs
@@ -0,0 +1,98 @@
+pub mod block {
+    pub fn practice() {
+        let x = {
+            let y = 10;
+            println!("y: {y}");
+            let z = {
+                let w = {
+                    3 + 4
+                };
+                println!("w: {w}");
+                y * w
+            };
+            println!("z: {z}");
+            z - y
+        };
+        println!("x: {x}");
+        let x = double(x);
+        println!("double x: {x}");
+    }
+
+    fn double(x: i32) -> i32 {
+        x + x
+    }
+}
+
+pub mod if_expression {
+    pub fn practice() {
+        let mut x = 10;
+        if x % 2 == 0 {
+            x /= 2;
+        } else {
+            x = 3 * x + 1;
+        }
+        println!("x: {x}");
+
+        let mut x = 10;
+        x = if x % 2 == 0 {
+            x / 2
+        } else {
+            3 * x + 1
+        };
+        println!("x: {x}")
+    }
+}
+
+pub mod for_loop {
+    pub fn practice() {
+        let v = vec![10, 20, 30];
+        for x in v {
+            println!("x: {x}");
+        }
+        println!();
+
+        for i in (0..10).step_by(2) {
+            println!("i: {i}");
+        }
+    }
+}
+
+pub mod while_loop {
+    pub fn practice() {
+        let mut x = 10;
+        while x != 1 {
+            println!("x: {x}");
+            x = if x % 2 == 0 { x / 2 } else { 3 * x + 1 };
+        }
+        println!("x: {x}");
+    }
+}
+
+pub mod break_and_continue {
+    pub fn practice() {
+        let v = vec![10, 20, 30];
+        let mut iter = v.into_iter();
+        'outer: while let Some(x) = iter.next() {
+            println!("x: {x}");
+            let mut i = 0;
+            while i < x {
+                println!("x: {x}, i: {i}");
+                i += 1;
+                if i == 3 {
+                    continue 'outer;
+                }
+            }
+        }
+    }
+}
+
+pub mod loop_expression {
+    pub fn practice() {
+        let mut x = 10;
+        loop {
+            x = if x % 2 == 0 { x / 2 } else { 3 * x + 1 };
+            println!("x: {x}");
+            if x == 1 { break; }
+        }
+    }
+}
