392854279d691945fb3f8063b7377bd08668985c
oyealex <oyealex@outlook.com>
2024-01-23 16:31:44 +0000

    multiple thread demo

--- rust_demo/practice/comprehensive_rust/src/main.rs
+++ rust_demo/practice/comprehensive_rust/src/main.rs
@@ -3,11 +3,9 @@ mod c1_basis;
 mod c2_control_flow;
 mod c3_variable;
 mod c4_enum;
+mod thread;
 
 // https://google.github.io/comprehensive-rust/zh-CN/index.html
 fn main() {
-    // c3_variable::variable_type::practice();
-    // c3_variable::static_and_const::practice();
-    // c4_enum::payload::practice();
-    c4_enum::layout::practice();
+    thread::test::practice();
 }
