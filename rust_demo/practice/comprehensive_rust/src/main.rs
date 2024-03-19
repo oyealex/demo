a6404bb0a6059ab0162e316d998dbfa153b88675
oyealex <oyealex@outlook.com>
2024-03-19 17:58:53 +0100

    add struct and reference test

--- rust_demo/practice/comprehensive_rust/src/main.rs
+++ rust_demo/practice/comprehensive_rust/src/main.rs
@@ -1,11 +1,13 @@
 #![allow(dead_code)] // 注意添加感叹号表示是一个crate级别的属性，否则为就近元素的属性
-mod c1_basis;
-mod c2_control_flow;
-mod c3_variable;
-mod c4_enum;
+mod basis;
+mod control_flow;
+mod variable;
+mod enums;
 mod thread;
+mod references;
+mod structs;
 
 // https://google.github.io/comprehensive-rust/zh-CN/index.html
 fn main() {
-    c2_control_flow::multi_table_9_9::practice();
+    structs::normal_struct::practice();
 }
